82e52e7e698849f3518c1a372a783e45def930a0
navghan099 <105035184+navghan099@users.noreply.github.com>
2024-07-29 23:21:03 +0530

    Add files via upload

---
+++ list_and_tuples.py
@@ -0,0 +1,22 @@
+# li = [1,4,7,8,4,2,9,3,5]
+# li.sort() #separately needed otherwise it return none 
+# print(li)
+# li.reverse()
+# print(li)
+# li.append(67) #should be appended at the last of the list
+# print(li)
+# li.insert(5,"navghan")
+# print(li)
+# li.pop(5)
+# print(li)
+# li.remove(67)
+# print(li)
+# print(li[0])
+
+
+# Tuples
+tuple = ("navghan",23,"bhoi",34,1,2,3,4,5,6,6,7,8)
+print(tuple)
+print(tuple[2:4])
+print(tuple.count(6))
+print(tuple.index(4))
